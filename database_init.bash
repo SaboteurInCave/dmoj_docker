89ff9dd581dda92036689834a89a6c9215fe30f8
Sergey <saboteurincave@gmail.com>
2019-01-27 16:54:51 +0300

    still working on database initialization...

--- database_init.bash
+++ database_init.bash
@@ -1,6 +1,6 @@
-docker exec dmoj mysql -uroot --execute='CREATE DATABASE dmoj DEFAULT CHARACTER SET utf8mb4 DEFAULT COLLATE utf8mb4_general_ci; GRANT ALL PRIVILEGES ON dmoj.* to 'dmoj'@'localhost' IDENTIFIED BY "${DATABASE_PASSWORD}"'
+docker exec dmoj --env-file=.env mysql -uroot --execute='CREATE DATABASE dmoj DEFAULT CHARACTER SET utf8mb4 DEFAULT COLLATE utf8mb4_general_ci; GRANT ALL PRIVILEGES ON dmoj.* to 'dmoj'@'localhost' IDENTIFIED BY "${DATABASE_PASSWORD}"'
 
-docker exec dmoj /bin/sh -c  cd /opt/dmoj/site;
+docker exec dmoj --env-file=.env /bin/sh -c  cd /opt/dmoj/site;
 python manage.py migrate;
 python manage.py loaddata navbar;
 python manage.py loaddata language_small;
